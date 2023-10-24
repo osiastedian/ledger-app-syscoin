d6e41e6a4de627e3a1bfbd903ccbfdcd92c93ddd
Ted Ian Osias <osiastedian@gmail.com>
2023-10-24 09:54:46 +0800

    a

--- tests_mainnet/test_bip86.py
+++ tests_mainnet/test_bip86.py
@@ -13,11 +13,13 @@ def test_bip86(client: Client, speculos_globals: SpeculosGlobals):
 
     # test for a native taproot wallet (bech32m addresses, per BIP-0086)
 
+    print(f"[{fpr}/86'/57'/0']xpub6Dzb9DcvJAhaNQwjeV9vg4SSVR6bwmVYQmrsc29FNZtKmLJcnosH5cg8P64a6qyhqvQtaoripUhCBTfjTcpbu4UaAGog2hcgmRq119u65uG")
+
     wallet = WalletPolicy(
         name="",
         descriptor_template="tr(@0/**)",
         keys_info=[
-            f"[{fpr}/86'/57'/0']xpub6CXaPZmr2vSHZwkubMWbGMhV2512eXPaEPnYLyfuJou6QU6pk1w5WsEPCkqTNJRNATxZ3eHT2xtm3Y3VkbvbSxu3rasjdTNv3twG2Yvvv6W",
+            f"[{fpr}/86'/57'/0']xpub6Dzb9DcvJAhaNQwjeV9vg4SSVR6bwmVYQmrsc29FNZtKmLJcnosH5cg8P64a6qyhqvQtaoripUhCBTfjTcpbu4UaAGog2hcgmRq119u65uG",
         ],
     )
 
